4a783878c802f7566a8a5ab5a96bbe90fe7d0b69
Aline <alinegoppinger.pf009@acacdemico.ifsul.edu.br>
2022-10-05 19:27:08 -0300

    correct name

--- FundamentosJava/src/fundamentos/operadores/OperadoresRelacionais.java
+++ FundamentosJava/src/fundamentos/operadores/OperadoresRelacionais.java
@@ -1,6 +1,6 @@
-package fundamentos.operdoresLogicos;
+package fundamentos.operadores;
 
-public class TiposLogicos {
+public class OperadoresRelacionais {
   public static void main(String[] args) {
 
     int i1 = 10;
